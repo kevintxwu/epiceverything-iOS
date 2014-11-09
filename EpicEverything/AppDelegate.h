24609b342127bf1728f9e23d89c6a715fc29e6a4
Varun Rau <varunrau@gmail.com>
2014-11-08 20:15:12 -0800

    created app

---
+++ EpicEverything/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EpicEverything
+//
+//  Created by Varun Rau on 11/8/14.
+//  Copyright (c) 2014 Unboard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
