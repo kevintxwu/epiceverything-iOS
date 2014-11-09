70bd570fae5369623402c6ce497cf31f2242a6f4
Varun Rau <varunrau@gmail.com>
2014-11-08 21:18:11 -0800

    added some controllers

---
+++ EpicEverything/UBMenuViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EpicEverything
+//
+//  Created by Varun Rau on 11/8/14.
+//  Copyright (c) 2014 Unboard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UBMenuViewController : UIViewController
+
+
+@end
+
---
+++ EpicEverything/UBMenuViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  EpicEverything
+//
+//  Created by Varun Rau on 11/8/14.
+//  Copyright (c) 2014 Unboard. All rights reserved.
+//
+
+#import "UBMenuViewController.h"
+#import "GameViewController.h"
+
+@interface UBMenuViewController ()
+
+@end
+
+@implementation UBMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)startGamePressed:(id)sender {
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
