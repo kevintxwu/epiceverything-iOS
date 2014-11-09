f4279976583b4fbb79c06cab5eb0e6aeb19a96e3
Varun Rau <varunrau@gmail.com>
2014-11-08 21:27:09 -0800

    added extensions

---
+++ EpicEverything/UIView+UBView.h
@@ -0,0 +1,16 @@
+//
+//  UIView+UBView.h
+//  EpicEverything
+//
+//  Created by Varun Rau on 11/8/14.
+//  Copyright (c) 2014 Unboard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIColor+UBColors.h"
+#import "UIImage+UBImage.h"
+#import <Masonry/Masonry.h>
+
+@interface UIView (UBView)
+
+@end
---
+++ EpicEverything/UIView+UBView.m
@@ -0,0 +1,13 @@
+//
+//  UIView+UBView.m
+//  EpicEverything
+//
+//  Created by Varun Rau on 11/8/14.
+//  Copyright (c) 2014 Unboard. All rights reserved.
+//
+
+#import "UIView+UBView.h"
+
+@implementation UIView (UBView)
+
+@end
